76f7196ca0b1571db386ccceddcc9a60a400c74f
Judson Powers <judson.powers@gmail.com>
2024-12-01 13:56:22 -0500

    Add comments.

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -3,9 +3,13 @@ use itertools::Itertools;
 
 advent_of_code::solution!(1);
 
+const INPUT_SIZE: usize = 1000;
+
 pub fn part_one(input: &str) -> Option<u64> {
-    let mut left = Vec::with_capacity(1000);
-    let mut right = Vec::with_capacity(1000);
+    let mut left = Vec::with_capacity(INPUT_SIZE);
+    let mut right = Vec::with_capacity(INPUT_SIZE);
+    // Each line is formatted as "<number> <number>".
+    // Split these into two vectors of numbers, one for each column.
     input
         .lines()
         .map(|s| s.split_ascii_whitespace().next_tuple().unwrap())
@@ -14,8 +18,10 @@ pub fn part_one(input: &str) -> Option<u64> {
             left.push(a);
             right.push(b);
         });
+    // Sort the columns.
     left.sort();
     right.sort();
+    // The solution is the sum of pairwise absolute differnces in the sorted lists.
     let result = left
         .iter()
         .zip(right.iter())
@@ -25,8 +31,10 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let mut left = Vec::with_capacity(1000);
-    let mut right: Counter<i64, u64> = Counter::with_capacity(1000);
+    let mut left = Vec::with_capacity(INPUT_SIZE);
+    let mut right: Counter<i64, u64> = Counter::with_capacity(INPUT_SIZE);
+    // Parse the two columns, as in Part 1.
+    // For the right-hand column, track the number of times each value appears.
     input
         .lines()
         .map(|s| s.split_ascii_whitespace().next_tuple().unwrap())
@@ -35,6 +43,8 @@ pub fn part_two(input: &str) -> Option<u64> {
             left.push(a);
             right[&b] += 1;
         });
+    // The solution is the sum of each element in the left-hand column
+    // multiplied by the number of times it appears in the right-hand column.
     let result = left.iter().map(|v| (*v as u64) * right[v]).sum();
     Some(result)
 }
