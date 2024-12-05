fe5a1aa65be42f7c67e9715d0e06834fbe528a5b
Judson Powers <judson.powers@gmail.com>
2024-12-05 13:32:36 -0500

    Change day 5 part 2 to use built-in sorting.

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Based on the glorious [template](https://github.com/fspoettel/advent-of-code-rus
 | [Day 2](./src/bin/02.rs) | `32.9µs` | `61.9µs` |
 | [Day 3](./src/bin/03.rs) | `164.8µs` | `221.3µs` |
 | [Day 4](./src/bin/04.rs) | `307.9µs` | `73.4µs` |
-| [Day 5](./src/bin/05.rs) | `112.2µs` | `728.4µs` |
+| [Day 5](./src/bin/05.rs) | `107.4µs` | `140.6µs` |
 
-**Total: 1.76ms**
+**Total: 1.16ms**
 <!--- benchmarking table --->
--- src/bin/05.rs
+++ src/bin/05.rs
@@ -6,48 +6,40 @@ advent_of_code::solution!(5);
 
 const MAX_PAGES: usize = 100;
 
-struct Rule {
-    before: u32,
-    after: u32,
-}
-
 struct RuleMap(HashMap<u32, BitSet>);
 
 type Update = Vec<u32>;
 
 struct Input {
-    rules: Vec<Rule>,
+    rules: RuleMap,
     updates: Vec<Update>,
 }
 
 fn parse_input(input: &str) -> Input {
     let mut lines = input.lines();
-    let mut rules = Vec::new();
-    loop {
-        let line = lines.next().unwrap();
-        if line.is_empty() {
-            break;
-        }
-        let (before, after) = line.split_once('|').unwrap();
-        rules.push(Rule {
-            before: before.parse().unwrap(),
-            after: after.parse().unwrap(),
-        });
-    }
+
+    let rules = RuleMap::from_iter(lines.by_ref().take_while(|line| !line.is_empty()).map(
+        |line| {
+            let (before, after) = line.split_once('|').unwrap();
+            (after.parse().unwrap(), before.parse().unwrap())
+        },
+    ));
+
     let updates = lines
         .map(|line| line.split(',').map(|s| s.parse().unwrap()).collect())
         .collect();
+
     Input { rules, updates }
 }
 
-fn update_is_valid(update: &Update, rule_map: &RuleMap) -> bool {
+fn update_is_valid(update: &Update, rules: &RuleMap) -> bool {
     let mut disallowed_values: BitSet = BitSet::with_capacity(MAX_PAGES);
 
     for &val in update.iter() {
         if disallowed_values.contains(val as usize) {
             return false;
         }
-        if let Some(new_disallowed) = rule_map.get(&val) {
+        if let Some(new_disallowed) = rules.get(&val) {
             disallowed_values.union_with(new_disallowed);
         }
     }
@@ -55,24 +47,19 @@ fn update_is_valid(update: &Update, rule_map: &RuleMap) -> bool {
     true
 }
 
-fn reorder_pages(update: &Update, rules: &[Rule]) -> Update {
-    let mut reordered = Vec::with_capacity(update.len());
-
-    for &value in update {
-        let applicable_rules = rules
-            .iter()
-            .filter(|rule| rule.before == value)
-            .collect::<Vec<_>>();
-        let index = applicable_rules
-            .iter()
-            .filter_map(|rule| reordered.iter().position(|&x| x == rule.after))
-            .min();
-        if let Some(index) = index {
-            reordered.insert(index, value);
+fn reorder_pages(update: &Update, rules: &RuleMap) -> Update {
+    let mut reordered = update.clone();
+    reordered.sort_by(|a, b| {
+        if let Some(rule) = rules.get(a) {
+            if rule.contains(*b as usize) {
+                std::cmp::Ordering::Less
+            } else {
+                std::cmp::Ordering::Greater
+            }
         } else {
-            reordered.push(value);
+            std::cmp::Ordering::Greater
         }
-    }
+    });
 
     reordered
 }
@@ -80,11 +67,9 @@ fn reorder_pages(update: &Update, rules: &[Rule]) -> Update {
 pub fn part_one(input: &str) -> Option<u64> {
     let Input { rules, updates } = parse_input(input);
 
-    let rule_map = RuleMap::from_iter(rules.iter().map(|rule| (rule.after, rule.before)));
-
     let result = updates
         .iter()
-        .filter(|update| update_is_valid(update, &rule_map))
+        .filter(|update| update_is_valid(update, &rules))
         .map(|update| update[update.len() / 2] as u64)
         .sum();
 
@@ -94,11 +79,9 @@ pub fn part_one(input: &str) -> Option<u64> {
 pub fn part_two(input: &str) -> Option<u64> {
     let Input { rules, updates } = parse_input(input);
 
-    let rule_map = RuleMap::from_iter(rules.iter().map(|rule| (rule.after, rule.before)));
-
     let result = updates
         .iter()
-        .filter(|update| !update_is_valid(update, &rule_map))
+        .filter(|update| !update_is_valid(update, &rules))
         .map(|update| reorder_pages(update, &rules))
         .map(|update| update[update.len() / 2] as u64)
         .sum();
@@ -117,6 +100,11 @@ impl RuleMap {
 
     fn get(&self, key: &u32) -> Option<&BitSet> {
         self.0.get(key)
+    }
+
+    #[allow(dead_code)]
+    fn len(&self) -> usize {
+        self.0.values().map(|v| v.len()).sum()
     }
 }
 
